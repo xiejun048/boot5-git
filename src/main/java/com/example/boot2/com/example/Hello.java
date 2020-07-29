429a15c99faa9effcd6bd1d1713dba7a00ab0006
xiejun048 <lujun19888@126.com>
2020-07-29 22:53:53 +0800

    项目经理第一次提交

---
+++ src/main/java/com/example/boot2/com/example/Hello.java
@@ -0,0 +1,18 @@
+package com.example.boot2.com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName : Hello
+ * @Description :
+ * @Author : jxie
+ * @Date: 2020-07-29 22:44
+ */
+@RestController
+public class Hello {
+    @RequestMapping("info")
+    public String say(){
+        return "hello word";
+    }
+}
